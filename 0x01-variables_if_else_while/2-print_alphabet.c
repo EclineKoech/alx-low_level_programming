913316743dd546d7e57106c492554fac59988aaf
Ecline Koech <eclinejerutokoech@gmail.com>
2022-03-15 02:45:35 +0300

    aprogram that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
-  int a = 'a';
-  while (a<= 'z'){
-    
-    putchar(a);
-    a++;
-  }
-
-  return(0);
+ int a = 'a';
+ while (a<= 'z'){
+   
+ putchar(a);
+ a++;
+ }
+return(0);
 }

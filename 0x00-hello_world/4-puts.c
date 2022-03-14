027e09dbf99ada27140e79a89c76bb8796a2180a
Ecline Koech <eclinejerutokoech@gmail.com>
2022-03-14 12:02:32 +0300

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a text
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 
 

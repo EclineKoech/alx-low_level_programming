ee79fb97351980e7ef83797418b2c728299ddfa0
Ecline Koech <eclinejerutokoech@gmail.com>
2022-03-19 02:50:27 +0300

    changes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,10 @@
 
 void print_numbers(void)
 {
-	int x;
-
-	for (x = '0'; x <= '9'; x++)
-	{
-		_putchar(x);
-	}
-	_putchar('\n');
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
 }

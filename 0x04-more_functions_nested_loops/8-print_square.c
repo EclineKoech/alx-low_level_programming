7a53749d102ba4de6f28a30e67ecc0ee3b394495
Ecline Koech <eclinejerutokoech@gmail.com>
2022-03-19 03:43:50 +0300

    changes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square
+ * @size: length and width of square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int r;
+int c;
+if (size > 0)
+{
+for (r = 0; r < size; r++)
+{
+for (c = 0; c < size; c++)
+{
+putchar('#');
+}
+putchar('\n');
+}
+}
+else
+{
+putchar('\n');
+}
+}
